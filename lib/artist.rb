f4710400804243429cf7240a090304d9d4da5e28
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 06:21:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,31 @@
-require 'pry'
+class Artist	
+  attr_accessor :name	
 
-class Artist
+  @@all = []	
 
-  attr_accessor :name, :songs
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
 
-  @@song_count = 0
+  def self.all	
+    @@all	
+  end	
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+  def add_song(song)	
+    song.artist = self	
+  end	
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
+  def add_song_by_name(name)	
+    song = Song.new(name)	
+    song.artist = self	
+  end	
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
+  def songs	
+    Song.all.select {|song| song.artist == self}	
+  end	
 
-  def self.song_count
-    @@song_count
-  end
-
+  def self.song_count	
+    Song.all.count	
+  end	
 end
